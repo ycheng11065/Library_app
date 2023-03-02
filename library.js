433423b42ddcf5751802a863bb6eeea4388e966e
Yu Cheng Li <ychengli11065@gmail.com>
2023-03-01 23:13:06 -0800

    Create the basic structure for the app

---
+++ library.js
@@ -0,0 +1,16 @@
+const library = [];
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
+
+const newBookbtn = document.querySelector("#new-book-btn");
+newBookbtn.addEventListener("click", () => {
+//   alert("HELLOW WORLD");
+  const newBookForm = document.querySelector("#new-book-form");
+  newBookForm.style.display = "block";
+});
